3c18d8dca77e1b72e4d10e2f4c579431681ee542
Krishna Pillutla <knpillutla@gmail.com>
2018-10-31 14:51:49 -0400

    Adding content

--- src/main/java/com/threedsoft/user/dto/responses/FieldResource.java
+++ src/main/java/com/threedsoft/user/dto/responses/FieldResource.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @NoArgsConstructor
@@ -20,7 +21,22 @@ public class FieldResource {
 	String fieldDisplayName;
 	String disableField;
 	String hideField;
-	String fieldType;
+	String fieldDataType;
 	String fieldLength;
 	String allowedValues;
+	String displayOptions;
+	String fieldType;
+
+	public FieldResource(String fieldName, String fieldDisplayName, String disableField, String hideField,
+			String fieldDataType, String fieldLength, String allowedValues, String displayOptions) {
+		this.fieldName=fieldName;
+		this.fieldDisplayName=fieldDisplayName;
+		this.disableField=disableField;
+		this.hideField=hideField;
+		this.fieldDataType=fieldDataType;
+		this.fieldLength=fieldLength;
+		this.allowedValues=allowedValues;
+		this.displayOptions=displayOptions;
+		this.fieldType="text";
+	}
 }
