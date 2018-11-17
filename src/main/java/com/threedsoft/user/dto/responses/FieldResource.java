e277347f4860ed0326a9b4755cafef90a89fe49a
Krishna Pillutla <knpillutla@gmail.com>
2018-11-17 00:26:56 -0500

    Adding

--- src/main/java/com/threedsoft/user/dto/responses/FieldResource.java
+++ src/main/java/com/threedsoft/user/dto/responses/FieldResource.java
@@ -1,8 +1,5 @@
 package com.threedsoft.user.dto.responses;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonInclude;
